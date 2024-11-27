1c1658bc56c09697c6bdedd57b95a5b0c4715c90
Ramith Hettiarachchi <ramith@cmu.edu>
2024-11-26 19:47:32 -0500

    add 1 user to exclude list & update filters

--- server/data_filter.py
+++ server/data_filter.py
@@ -8,10 +8,10 @@ from server.database import db, Post
 
 ML_PATTERN = re.compile(r'(?i)(?:\b(?:machine|deep|geometric\s+deep)\s+learning\b|bioML|\b(?:neural\s+network(?:s)?|graph\s+neural\s+network(?:s)?|(?:protein\s+)?language\s+model(?:s)?|(?:ESM)-?\d*|(?:prot(?:BERT|einMPNN)|openFold|helixFold)|(?:GNINA|VINA)|flow-matching|boltz-\d*|diffusion\s+model(?:s)?|ColabFold|(?:pLM)s?|transformer(?:s)?|(?:LIGO|RFdiffusion|RoseTTAFold)|alphafold|AF[2-3]|GNN|VAE|ESMFold|OmegaFold|ProstQA)(?:\s*-?\s*(?:predicted|prediction|predictions))?\b|\bexplainable\b|\battention mechanism\b|\bfoundation model\b|\bfine-tuning\b|\bembedding\b|artificial intelligence|self-supervised|context-aware|context aware|zero-shot|pretraining|auxiliary tasks|latent space|equivariant|invariant|tensor-based|flow matching|optimal transport|featurisation|reinforcement learning|diffusion|active learning|masked modeling|inverse folding|representation learning|contrastive learning|linear probe|\bMCMC\b|generative model|\bIsomorphic Labs\b|\bRecursion Pharmaceuticals?\b|\bExscientia\b|\bAtomwise\b|\bInsilico Medicine\b|\bIktos\b)')
 
-BIO_PATTERN = re.compile(r'(?i)(?:protein(s)?|enzyme(s)?|molecular biology|structural biology|\bpdb\b|peptide(s)?|amino acid(s)?|protein folding|molecular dynamic(s)?|molecular representation(s)?|ligand(s)? dock(ing|ed)|\bdock(ing|ed)?\b|\bmolecule representation\b|\bNMR\b|cryoem|cryo-em|conformation(al|s)?|\bbinding site(s)?\b|posebuster(s)?|\bkinase(s)?\b|\bmicrobio(logy|logical)?\b|\bBioML\b|genomic context(s)?|active site(s)?|enzyme design(s|ing)?|structural motif(s)?|protein sequence(s)?|structural proteome(s|ics)?|protein engineer(ing|ed)|protein.*?function(s|al)?|small molecule|protein.*?structure(s|al)?|\bPyMOL\b|\bChimeraX\b|\bVMD\b|\bstructural alignment(s)?\b|\bprotein-protein interaction(s)?\b|\bprotein complex(es)?\b|MMseqs|\bMSA\b|drug discovery|drug design|virtual screening|RNA structure|base pairs|pseudoknots|G-quadruplex|electrostatic potential|atomic model|biomolecule|macromolecule|\bPPI(s)?\b|crystallography|coevolution|phylogeny|\bside chain\b|backbone structure|backbone prediction|residue|biotherapeutics|protein design|binding affinity|binding pocket|protein surface|molecular surface|pharmacophore|TM[-\s]?score|[pl]?lddt|DrugDiscovery|DrugDesign|\bbinding\b)')
+BIO_PATTERN = re.compile(r'(?i)(?:protein(s)?|enzyme(s)?|molecular biology|structural biology|\bpdb\b|peptide(s)?|amino acid(s)?|protein folding|molecular dynamic(s)?|molecular representation(s)?|ligand(s)? dock(ing|ed)|\bdock(ing|ed)?\b|\bmolecule representation\b|\bNMR\b|cryoem|cryo-em|conformation(al|s)?|\bbinding site(s)?\b|posebuster(s)?|\bkinase(s)?\b|\bmicrobio(logy|logical)?\b|\bBioML\b|genomic context(s)?|active site(s)?|enzyme design(s|ing)?|structural motif(s)?|protein sequence(s)?|structural proteome(s|ics)?|protein engineer(ing|ed)|protein.*?function(s|al)?|small molecule|protein.*?structure(s|al)?|\bPyMOL\b|\bChimeraX\b|\bVMD\b|\bstructural alignment(s)?\b|\bprotein-protein interaction(s)?\b|\bprotein complex(es)?\b|MMseqs|\bMSA\b|drug discovery|drug design|virtual screening|\bRNA\b|RNA structure|base pairs|pseudoknots|G-quadruplex|electrostatic potential|atomic model|biomolecule|macromolecule|\bPPI(s)?\b|crystallography|coevolution|phylogeny|\bside chain\b|backbone structure|backbone prediction|residue|biotherapeutics|protein design|binding affinity|binding pocket|protein surface|molecular surface|pharmacophore|TM[-\s]?score|[pl]?lddt|DrugDiscovery|DrugDesign|\bbinding\b)')
 
 
-EXCLUDED_PATTERN = re.compile(r'(?i)\b(fuck(?:er|ing|ed|s)?|shit(?:ty|ting|ted|s)?|ass(?:hole|es|ed)?|bitch(?:es|ing|ed|y)?|cunt(?:s|ing|ed)?|dick(?:head|s|ed)?|bastard(?:s|ed)?|wank(?:er|ing|ed|s)?|twat(?:s|ted)?|whore(?:s|ing|ed)?|slut(?:ty|s)?|cock(?:s|ed)?|puss(?:y|ies)|turd(?:s)?|fag(?:got|s)?|prick(?:s|ed)?|retard(?:ed|s)?|bollock(?:s|ed)?|arse(?:s|hole|d)?|goddamn(?:ed|it)?|mother(?:fuck(?:er|ing))?|asshole(?:s)?|bullshit(?:ting|ted)?|porn(?:o|ography)?|lesb(?:ian|o|y|ians)?|gay(?:s)?|queer|homo(?:sexual)?|faggot(?:s)?|dyke(?:s)?|nigger(?:s)?|kike(?:s)?|spic(?:s)?|wetback(?:s)?|chink(?:s)?|paki(?:s)?|raghead(?:s)?|towelhead(?:s)?|tit(?:s|ties|ty)?|pony)\b')
+EXCLUDED_PATTERN = re.compile(r'(?i)\b(fuck(?:er|ing|ed|s)?|shit(?:ty|ting|ted|s)?|ass(?:hole|es|ed)?|bitch(?:es|ing|ed|y)?|cunt(?:s|ing|ed)?|dick(?:head|s|ed)?|bastard(?:s|ed)?|wank(?:er|ing|ed|s)?|twat(?:s|ted)?|whore(?:s|ing|ed)?|slut(?:ty|s)?|cock(?:s|ed)?|puss(?:y|ies)|turd(?:s)?|fag(?:got|s)?|prick(?:s|ed)?|retard(?:ed|s)?|bollock(?:s|ed)?|arse(?:s|hole|d)?|goddamn(?:ed|it)?|mother(?:fuck(?:er|ing))?|asshole(?:s)?|bullshit(?:ting|ted)?|porn(?:o|ography)?|lesb(?:ian|o|y|ians)?|gay(?:s)?|queer|homo(?:sexual)?|faggot(?:s)?|dyke(?:s)?|nigger(?:s)?|kike(?:s)?|spic(?:s)?|wetback(?:s)?|chink(?:s)?|paki(?:s)?|raghead(?:s)?|towelhead(?:s)?|tit(?:s|ties|ty)?|pony|\bintimate\b)\b')
 EXCLUDED_PATTERN_2 = re.compile(r'(?i)(whey\s+(?:protein|powder)|protein\s+powder)')
 
 import json
@@ -21,6 +21,8 @@ with open('/home/ruh/www/mlsb_feed_hosted/server/config_users.json', 'r') as con
 
 AUTO_INCLUDE_DIDS = set(config.get('auto_include_dids', []))
 BIO_ONLY_DIDS = set(config.get('bio_only_dids', []))
+
+EXCLUDE_DIDS = set(config.get('exclude_dids', []))
 
 def is_relevant_post(text: str, author_did: str) -> bool:
     """
@@ -34,6 +36,9 @@ def is_relevant_post(text: str, author_did: str) -> bool:
         bool: True if the post is relevant, False otherwise.
     """
     # Auto-included users: Include all posts regardless of content
+    if(author_did in EXCLUDE_DIDS):
+        return False
+
     if author_did in AUTO_INCLUDE_DIDS:
         return True
 
