70ddf1f400e25ae0a931ed08045ef5f2d30e4438
Ramith Hettiarachchi <ramith@cmu.edu>
2024-11-28 23:43:28 -0500

    add logo

--- publish_feed.py
+++ publish_feed.py
@@ -6,7 +6,7 @@ from atproto import Client, models
 
 # YOUR bluesky handle
 # Ex: user.bsky.social
-HANDLE: str = ''
+HANDLE: str = 'ramith.fyi'
 
 # YOUR bluesky password, or preferably an App Password (found in your client settings)
 # Ex: abcd-1234-efgh-5678
@@ -14,24 +14,24 @@ PASSWORD: str = ''
 
 # The hostname of the server where feed server will be hosted
 # Ex: feed.bsky.dev
-HOSTNAME: str = ''
+HOSTNAME: str = 'mlsb.ramith.io'
 
 # A short name for the record that will show in urls
 # Lowercase with no spaces.
 # Ex: whats-hot
-RECORD_NAME: str = ''
+RECORD_NAME: str = 'MLSB'
 
 # A display name for your feed
 # Ex: What's Hot
-DISPLAY_NAME: str = ''
+DISPLAY_NAME: str = 'ML in Structural Biology'
 
 # (Optional) A description of your feed
 # Ex: Top trending content from the whole network
-DESCRIPTION: str = 'powered by The AT Protocol SDK for Python'
+DESCRIPTION: str = 'Latest posts on Machine Learning in Structural Biology (Reverse chronologically ordered).'
 
 # (Optional) The path to an image to be used as your feed's avatar
 # Ex: ./path/to/avatar.jpeg
-AVATAR_PATH: str = ''
+AVATAR_PATH: str = 'logo-mlsb.png'
 
 # (Optional). Only use this if you want a service did different from did:web
 SERVICE_DID: str = ''
